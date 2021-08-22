844c89af898cb38d7116a54bdc28843e274df8b1
Jonayed Mohiuddin <50599884+JonayedMohiuddin@users.noreply.github.com>
2021-08-23 00:15:23 +0600

    Add files via upload

---
+++ StrToBrainfuck.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+/* 
+When appending code by this method, this method must be called in a empty memory and another 
+empty memory must follow it. If you are overriding this shouldnt be a concern.
+Example: if called when pointing to mem[X] then mem[X] and mem[X+1] must be empty.
+*/
+
+string toBrainfuck(string str, bool append=false)
+{
+    string brainfuck;
+    char curChar;
+    int mul;
+    int rem;
+
+    if(!append)
+    {
+        brainfuck += "$ASCII\n"; 
+    }
+
+    brainfuck += "#CURRENTLY PTR AT MEM[X]"; 
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        curChar = (char)str[i];
+        mul = (int)curChar / 10;  // 65(A) -> 6, 72(H) -> 7
+        rem = (int)curChar % 10;  // 65(A) -> 5, 72(H) -> 2
+
+        brainfuck += "\n++++++++++[>"; // Boilerplate code: first part of loop. i=10
+
+        // Loops until successfully written char in memory
+        for (int i = 0; i < mul; i++)
+        {
+            brainfuck += "+";
+        }
+
+        brainfuck += "<-]>";
+
+        for (int i = 0; i < rem; i++)
+        {
+            brainfuck += "+";
+        }
+
+        brainfuck += ".<    ";
+        // Comments
+        brainfuck = brainfuck + "#STORE "+to_string((int)str[i])+"(";
+        
+        if(str[i] != '\n')  // printing \n breaks comments
+          brainfuck += str[i];
+        
+        brainfuck = brainfuck + ") IN MEM[X+1] AND PRINT IT";
+
+        // Clear the memory. So that it can be used later.
+
+        brainfuck += "\n++++++++++[>"; 
+
+        for (int i = 0; i < mul; i++)
+        {
+            brainfuck += "-";
+        }
+
+        brainfuck += "<-]>";
+
+        for (int i = 0; i < rem; i++)
+        {
+            brainfuck += "-";
+        }
+
+        brainfuck += "<     ";
+        brainfuck += "#CLEAR MEM[X+1] AND SET PTR AT MEM[X]\n";
+    }
+    
+    return brainfuck;
+}
+
+void writeToFile(string text, string filepath="temp.bf", bool append=false)
+{
+    filepath = "codes/" + filepath;
+    ofstream file;
+    file.open(filepath);
+    file << text;
+    file.close();
+}
+
+void readFile(string filepath, string *str)
+{
+  ifstream sourceFile;
+	sourceFile.open(filepath);
+	if(sourceFile)
+	{
+	  getline(sourceFile, *str, '\0');
+	}
+	else
+	{
+		cout << "~ ! No such file (" + filepath + ") in codes directory. Please add one first! \n";
+		cout << "~ ! Exiting now !\n";
+		exit(EXIT_FAILURE);
+	}
+	sourceFile.close();
+	*str += '\n';
+}
+
+int main(int argc, char const *argv[])
+{
+    string str, filename, brainfuck;
+    cout << "Input String:";
+    getline(cin, str, '\n');
+    cout << "Input filename(will be stored in codes dir. Default filename is temp.bf):\n";
+    getline(cin, filename);
+    
+    //readFile("codes/NewFile.txt", &str);
+    
+    brainfuck = toBrainfuck(str);
+    
+    cout << "\n\n";
+    cout << brainfuck;
+    cout << "\n\n";
+    writeToFile(brainfuck, filename);
+
+    return 0;
+}
+// 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
--- brainfuck.cpp
+++ brainfuck.cpp
@@ -4,95 +4,97 @@ using namespace std;
 
 class Reader
 {
-	private:
+private:
 	int curPos;
 	char curChar;
 	string source;
 	string filepath;
-	
-	public:
+
+public:
 	Reader()
 	{
 		source = " ";
 		curPos = -1;
 		nextChar();
 	}
-	
+
 	Reader(string filepath)
 	{
 		this->filepath = filepath;
-		
-    if(filepath == "" || filepath == " " || filepath == "\n")
+
+		if (filepath == "" || filepath == " " || filepath == "\n")
 		{
 			this->filepath += "codes/";
-    	cout << "Enter brainfuck code (or filename adding ~ before it):\n";
-    	cin >> source;
-    	
-    	if(source[0] == '~')
-    	{
-    		this->filepath += source.substr(1, source.length());
-    	  readFile();
-    	}
-    }
-    else
-    {
-      readFile();
-    }
-    
-	  curPos = -1;
-    nextChar();
+			cout << "Enter brainfuck code (or filename adding ~ before it):\n";
+			cin >> source;
+
+			if (source[0] == '~')
+			{
+				this->filepath += source.substr(1, source.length());
+				readFile();
+			}
+		}
+		else
+		{
+			readFile();
+		}
+
+		curPos = -1;
+		nextChar();
 	}
-	
+
 	void readFile()
 	{
 		ifstream sourceFile;
-    sourceFile.open(filepath);
-    if(sourceFile)
-    {
-      getline(sourceFile, source, '\0');
-    }
-    else
-    {
-      cout << "~ ! No such file ("+filepath+") in codes directory. Please add one first! " << endl;
-      cout << "~ ! Exiting now !" << endl;
-      exit(EXIT_FAILURE);
-    }
-    sourceFile.close();
-    
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // ! BUG     								 !
-    // ! Try running hello.bf    !
-    // ! Reads ascii of % as 239 !
-    // ! Reads A as 187					 !
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!
-    cout<<"\nSource:"<<source << endl;
-    cout<<(int)source[0] << endl;
-    cout<<(int)source[1] << endl;
+		sourceFile.open(filepath);
+		if (sourceFile)
+		{
+		  getline(sourceFile, source, '\0');
+		}
+		else
+		{
+			cout << "~ ! No such file (" + filepath + ") in codes directory. Please add one first! \n";
+			cout << "~ ! Exiting now !\n";
+			exit(EXIT_FAILURE);
+		}
+		sourceFile.close();
+		source += '\n';
 	}
-		
+
 	char getChar()
 	{
 		return curChar;
 	}
-	
+
 	void nextChar()
 	{
 		curChar = source[++curPos];
 	}
-	
+
 	void previousChar()
 	{
 		curChar = source[--curPos];
 	}
 	
-	// D=Decimal, A=ASCII, Default: D
+	int getCurPos()
+	{
+		return curPos;
+	}
+
+	// $D=Decimal, $A=ASCII, Default: D
 	char getOutputMode()
 	{
-		if(getChar() == '$')
+		while (getChar() != '$' && getChar() != '>' && getChar() != '<' && getChar() != '+' && getChar() != '-' && getChar() != '.' && getChar() != ',' && getChar() != '[' && getChar() != ']')
 		{
 			nextChar();
-		  return getChar();
 		}
+
+		if (getChar() == '$')
+		{
+			nextChar();
+			return getChar();
+		}
+		
 		else
 		{
 			return 'D';
@@ -100,123 +102,164 @@ class Reader
 	}
 };
 
+// This is the Brainfuck interpreter which runs Brainfuck codes from files or manual input.
+
 class Interpreter
 {
-	private:
-  int memory[1000];
-	int pointer;
+private:
+	char mem[3000] = {0};
+	int memSize; 
+	char *ptr;
+	char outputMode; // $A = ASCII or $D = DECIMAL
+	int loopDepth;   // 0 [ 1 [ 2 [ 3 ] 2 ] 1 ] 0
 	Reader reader;
-	char outputMode;
-	
-	public:
+
+public:
 	Interpreter(Reader reader)
 	{
-		this->reader = reader;
-		pointer = 0;
-		resetMem();
+		this->reader = reader;; 
+		ptr = mem;
+		memSize = 3000;
 		outputMode = reader.getOutputMode();
 		interpret();
 	}
 	
-	void resetMem()
-	{
-		for(int i = 0; i <= 1000; i++)
-		  memory[i] = 0;
-	}
-	
-	// Brainfuck memory cells are 8bit or 1byte width so they cant be 
-	// bigger or lesser than that.
-	int convertToByte()
-	{
-		if(memory[pointer] > 127)
-		  return memory[pointer] = 127;
-	  else if(memory[pointer] < 0)
-		  return memory[pointer] = 0;
-	  
-	  return memory[pointer];
-	}
-	
 	void interpret()
 	{
-		while(reader.getChar() != '\0')
+		while (reader.getChar() != '\0')
 		{
-	  	if(reader.getChar() == '<')
-	  	{
-	  		pointer--;
-	  		memory[pointer] = convertToByte();
-	  	}
-	  	else if(reader.getChar() == '>')
-	  	{
-		  	pointer++;
-		  	memory[pointer] = convertToByte();
-		  }
-		  else if(reader.getChar() == '+')
-	  	{
-		  	memory[pointer]++;
-		  	memory[pointer] = convertToByte();
-		  }
-		  else if(reader.getChar() == '-')
-	  	{
-		  	memory[pointer]--;
-		  	memory[pointer] = convertToByte();
-		  }
-		  else if(reader.getChar() == '.')
-	  	{
-	  		// %D means decimal output, %A means ASCII output
-	  		if(outputMode == 'D')
-		  	{
-		  	  cout << "VALUE OF " << pointer << " IS " << to_string(memory[pointer]) << endl;
-		  	}
-		  	else if(outputMode == 'A')
-		  	{
-		  		char output = (char)memory[pointer];
-		  		cout << output;
-		  	}
-		  }
-		  else if(reader.getChar() == ',')
-	  	{
-	  	  cout << "Input : ";
-	  		cin >> memory[pointer];
-	  		memory[pointer] = convertToByte();
-		  }
-		  // If memory currently pointing at is zero goto next ]
-		  else if(reader.getChar() == '[')
-	  	{
-	  		if(memory[pointer] == 0)
-	  		{
-	  			while(reader.getChar() != ']')
-	  			  reader.nextChar();
-	  		}
-		  }
-		  // If memory currently pointing at is non-zero then goto preceding [
-		  else if(reader.getChar() == ']')
-	  	{
-	  		if(memory[pointer] != 0)
-	  		{
-	  			while(reader.getChar() != '[')
-	  			  reader.previousChar();
-	  		}
-		  }
-		  else
-		  {
-		  	// Just do nothing if encountered un-recognised charecters
-		  }
+			if (reader.getChar() == '<')
+			{
+				--ptr;
+				if (ptr < mem)
+				{
+					ptr = &mem[memSize];
+				}
+			}
+			else if (reader.getChar() == '>')
+			{
+				++ptr;
+				if (ptr > &mem[3000])
+				{
+					ptr = mem;
+				}
+			}
+			else if (reader.getChar() == '+')
+			{
+				++*ptr;
+			}
+			else if (reader.getChar() == '-')
+			{
+				--*ptr;
+			}
+			else if (reader.getChar() == '.')
+			{
+				// $D means decimal output, $A means ASCII output
+				if (outputMode == 'D')
+				{
+					cout << "MEM[" << ptr - mem << "] = " << to_string((int)*ptr) << '\n';
+				}
+				else if (outputMode == 'A')
+				{
+					cout << *ptr;
+				}
+			}
+			else if (reader.getChar() == ',')
+			{
+				cout << "Input : ";
+				cin >> *ptr;
+			}
+			// If memory currently pointing at is zero goto next same depths ']'
+			else if (reader.getChar() == '[')
+			{
+				if ((int)*ptr == 0)
+				{
+					loopDepth = 1;
+					reader.nextChar();
+					
+					while (loopDepth != 0)
+					{
+						if(reader.getChar() == '[')
+							++loopDepth;
+						else if(reader.getChar() == ']')
+							--loopDepth;
+						
+						reader.nextChar();
+						
+						if(!reader.getChar() == '\0') // If EOF
+						{
+						  cout << "Error no matching closing ']' braces!";
+						  exit(EXIT_SUCCESS);
+						} 
+					} 
+				}
+			}
+			// If memory currently pointing at is non-zero then goto goto next same depths preceding '['
+			else if (reader.getChar() == ']')
+			{
+				if ((int)*ptr != 0)
+				{
+					loopDepth = 1;
+					reader.previousChar();
+					
+					while (loopDepth != 0) 
+					{
+						if(reader.getChar() == ']')
+							++loopDepth;
+						else if(reader.getChar() == '[')
+							--loopDepth;
+						
+						reader.previousChar();
+						
+						if(reader.getChar() == '\0') // If EOF
+						{
+						  cout << "Error no matching opening '[' braces!";
+						  exit(EXIT_FAILURE);
+						} 
+					}
+				}
+			}
+			// Just skip comments starting with # (Not a brainfuck directive)
+			else if (reader.getChar() == '#')
+			{
+				while (reader.getChar() != '\n')
+					reader.nextChar();
+			}
 		  
-		  reader.nextChar();
+			reader.nextChar();
 		}
 	}
 };
 
+void printHelp()
+{
+	cout << "=========================\n";
+	cout << "= BRAINFUCK INTERPRETER =\n";
+	cout << "=========================\n";
+	cout << "# This interpreter can directly run codes from your input or filename given.\n";
+	cout << "# This interpreter searches for brainfuck files (*.bf) in codes directory.\n";
+	cout << "# If inputting filenames it must be followed by a ~ sign.\n";
+	cout << "# For inputting code just type it directly.\n";
+	cout << "# $ is output mode controller.\n";
+	cout << "# Write $ASCII or $A at the start of line for ASCII output.\n";
+	cout << "# Write $DECIMAl or $D at the start of line forDECIMAL output.\n";
+	cout << "\n";
+}
+
 int main(int argc, char *argv[])
 {
 	string filepath = "codes/";
-  if(argc >= 2)
-    filepath += argv[1];
-  else
-    filepath = "";
-  
+	if (argc >= 2)
+		filepath += argv[1];
+	else
+	{
+	  printHelp();
+		filepath = "";
+	}
+
 	Reader reader(filepath);
-	Interpreter	interpreter(reader);
-	
+	Interpreter interpreter(reader);
+	cout << '\n';
+
 	return 0;
 }
